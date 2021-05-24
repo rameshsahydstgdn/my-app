65e67e1c259f3ddbf42346be5fa531aff468624d
Ramesh Kumar <ramesh.sa.hydstgdn@gmail.com>
2021-05-25 00:52:33 +0530

    second commit

--- src/routes/posts/[post_id]/index.json.js
+++ src/routes/posts/[post_id]/index.json.js
@@ -2,8 +2,9 @@ import * as api from '$lib/api.js';
 
 export async function get({ params }) {
   const { post_id } = params;
-  const { post } = await api.get(`posts/${post_id}`);
-
+  console.log(post_id);
+  const post = await api.get(`posts/${post_id}`);
+  console.log(post);
   return {
     body: post,
   };
--- src/routes/posts/[post_id]/index.svelte
+++ src/routes/posts/[post_id]/index.svelte
@@ -1,10 +1,17 @@
 <script context="module">
   export async function load({ page, fetch }) {
     const { post_id } = page.params;
-
-    const post = await fetch(`/bag/${post_id}.json`).then((r) => {
-      return r.json();
-    });
+    const post = await fetch(`${post_id}.json`)
+      .then((r) => {
+        if (r.status !== 200) {
+          throw new Error(`There was an error with status code ${r.status}`);
+        }
+        return r.json();
+      })
+      .then((r) => {
+        return r;
+      });
+    console.log(post);
     return {
       props: { post },
     };
@@ -15,4 +22,4 @@
   export let post;
 </script>
 
-<p>{post}</p>
+<pre>{post.body}</pre>
